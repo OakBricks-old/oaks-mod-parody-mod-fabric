fa7af3cf9253ccb0c711cd9f6279a8bff233c1a4
OakBricks <oakbricks@gmail.com>
2021-06-24 18:23:06 -0500

    Throw runtime exception at server launch

---
+++ src/main/java/com/poophater/mod/PoophatersModServer.java
@@ -0,0 +1,13 @@
+package com.poophater.mod;
+
+import net.fabricmc.api.DedicatedServerModInitializer;
+
+public class PoophatersModServer implements DedicatedServerModInitializer {
+    /**
+     * Runs the mod initializer on the server environment.
+     */
+    @Override
+    public void onInitializeServer() {
+        throw new RuntimeException();
+    }
+}
